8fb450074461268e347c2d19f263c04e9acdc7f7
Halvor <halvor.kvamme@gmail.com>
2022-12-01 13:26:23 +0100

    Replace 2022 - 1 - 2 with slightly faster version

--- src/year_2022/day01.rs
+++ src/year_2022/day01.rs
@@ -21,8 +21,31 @@ impl AdventSolver for DayOne {
         highest.into()
     }
 
+    // fn part_two(&self, input: &str) -> Solution {
+    //     let mut cals = Vec::new();
+    //     let mut counter = 0;
+    //     for line in input.lines() {
+    //         match line.parse::<u32>() {
+    //             Ok(v) => {
+    //                 counter += v;
+    //             }
+    //             Err(_) => {
+    //                 cals.push(counter);
+    //                 counter = 0;
+    //             }
+    //         }
+    //     }
+
+    //     cals.sort();
+    //     let mut res = 0;
+    //     for _ in 0..3 {
+    //         res += cals.pop().unwrap();
+    //     }
+
+    //     res.into()
+    // }
     fn part_two(&self, input: &str) -> Solution {
-        let mut cals = Vec::new();
+        let mut cals = [0_u32; 3];
         let mut counter = 0;
         for line in input.lines() {
             match line.parse::<u32>() {
@@ -30,19 +53,17 @@ impl AdventSolver for DayOne {
                     counter += v;
                 }
                 Err(_) => {
-                    cals.push(counter);
+                    let smallest = cals.iter().min().unwrap();
+                    if counter > *smallest {
+                        if let Some(index) = cals.iter().position(|x| smallest == x) {
+                            cals[index] = counter;
+                        };
+                    }
                     counter = 0;
                 }
             }
         }
-
-        cals.sort();
-        let mut res = 0;
-        for _ in 0..3 {
-            res += cals.pop().unwrap();
-        }
-
-        res.into()
+        cals.iter().sum::<u32>().into()
     }
 
     fn visualize(&self, _input: &str) {}

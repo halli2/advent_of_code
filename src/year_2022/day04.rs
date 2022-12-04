d87b73b7c51efb34a7d33b89306fab23ad874f83
Halvor <halvor.kvamme@gmail.com>
2022-12-04 14:27:59 +0100

    2022 04 much cleaner

--- src/year_2022/day04.rs
+++ src/year_2022/day04.rs
@@ -1,18 +1,16 @@
-use std::cmp::Ordering;
+use std::{cmp::Ordering, ops::Range};
 
 #[cfg(test)]
 use crate::bench;
 use crate::{AdventSolver, Solution};
 pub struct DayFour {}
 
-fn section(s: &str) -> (u32, u32) {
+fn section(s: &str) -> Range<u32> {
     s.split_once('-')
-        .map(|(left, right)| (left.parse::<u32>().unwrap(), right.parse::<u32>().unwrap()))
+        .map(|(left, right)| (left.parse::<u32>().unwrap()..right.parse::<u32>().unwrap()))
         .unwrap()
 }
-
-/// Section1 (u32..u32), section2 (u32..u32)
-fn sections(s: &str) -> ((u32, u32), (u32, u32)) {
+fn sections(s: &str) -> (Range<u32>, Range<u32>) {
     s.split_once(',')
         .map(|(lhs, rhs)| (section(lhs), section(rhs)))
         .unwrap()
@@ -24,25 +22,12 @@ impl AdventSolver for DayFour {
             .lines()
             .into_iter()
             .map(sections)
-            .map(|((left_1, left_2), (right_1, right_2))| {
-                match left_1.cmp(&right_1) {
-                    Ordering::Greater => {
-                        if left_2 <= right_2 {
-                            return 1;
-                        }
-                    }
-                    Ordering::Less => {
-                        if left_2 >= right_2 {
-                            return 1;
-                        }
-                    }
-                    Ordering::Equal => {
-                        return 1;
-                    }
-                }
-                0
+            .filter(|(left, right)| match left.start.cmp(&right.start) {
+                Ordering::Greater => left.end <= right.end,
+                Ordering::Less => left.end >= right.end,
+                Ordering::Equal => true,
             })
-            .sum::<u32>()
+            .count()
             .into()
     }
 
@@ -51,31 +36,18 @@ impl AdventSolver for DayFour {
             .lines()
             .into_iter()
             .map(sections)
-            .map(|((left_1, left_2), (right_1, right_2))| {
-                match left_1.cmp(&right_1) {
-                    Ordering::Greater => {
-                        if left_1 <= right_2 {
-                            return 1;
-                        }
-                    }
-                    Ordering::Less => {
-                        if left_2 >= right_1 {
-                            return 1;
-                        }
-                    }
-                    Ordering::Equal => {
-                        return 1;
-                    }
-                }
-                0
+            .filter(|(left, right)| match left.start.cmp(&right.start) {
+                Ordering::Greater => left.start <= right.end,
+                Ordering::Less => left.end >= right.start,
+                Ordering::Equal => true,
             })
-            .sum::<u32>()
+            .count()
             .into()
     }
 }
 
 #[cfg(test)]
-bench! {2022, 4, DayFour, year_2022, Solution::U32(496), Solution::U32(847)}
+bench! {2022, 4, DayFour, year_2022, Solution::Usize(496), Solution::Usize(847)}
 
 #[cfg(test)]
 mod tests {
@@ -90,14 +62,14 @@ mod tests {
 
     #[test]
     fn part1() {
-        let result = Solution::U32(2);
+        let result = Solution::Usize(2);
         let day = DayFour {};
         assert_eq!(day.part_one(INPUT), result);
     }
 
     #[test]
     fn part2() {
-        let result = Solution::U32(4);
+        let result = Solution::Usize(4);
         let day = DayFour {};
         assert_eq!(day.part_two(INPUT), result);
     }

396fb43172575e3fe730553776ce44c19b791640
Halvor <halvor.kvamme@gmail.com>
2022-12-08 15:33:08 +0100

    2022 08 - v2

--- src/year_2022/day08.rs
+++ src/year_2022/day08.rs
@@ -1,3 +1,5 @@
+use core::slice;
+
 #[cfg(test)]
 use crate::bench;
 use crate::{AdventSolver, Solution};
@@ -5,65 +7,56 @@ pub struct DayEight {}
 
 impl AdventSolver for DayEight {
     fn part_one(&self, input: &str) -> Solution {
-        // let input = input.as_bytes();
-        let trees: Vec<Vec<u8>> = input
-            .lines()
-            .map(|lines| lines.as_bytes().iter().map(|b| (b & 0x0f) + 1).collect())
-            .collect();
-        // let width = slice::memchr::memchr(b'\n', input).unwrap();
-        let mut grid = vec![vec![0; trees.len()]; trees.len()];
-        let mut highest_up: [u8; 128] = [0; 128];
-        let mut highest_down: [u8; 128] = [0; 128];
-        let len = trees.len();
+        let trees = input.as_bytes();
+        let len = slice::memchr::memchr(b'\n', trees).unwrap();
+        let width = len + 1;
+        let mut grid = [[false; 99]; 99];
+        let mut highest_up: [u8; 99] = [0; 99];
+        let mut highest_down: [u8; 99] = [0; 99];
         for i in 0..len {
             let mut highest_left = 0;
             let mut highest_right = 0;
             for j in 0..len {
-                let tree = trees[i][j];
+                let tree = trees[i * width + j];
                 if tree > highest_left {
-                    grid[i][j] = 1;
+                    grid[i][j] = true;
                     highest_left = tree;
                 }
                 if tree > highest_up[j] {
-                    grid[i][j] = 1;
+                    grid[i][j] = true;
                     highest_up[j] = tree;
                 }
 
-                let tree = trees[len - i - 1][j];
+                let tree = trees[(len - i - 1) * width + j];
                 if tree > highest_down[j] {
-                    grid[len - i - 1][j] = 1;
+                    grid[len - i - 1][j] = true;
                     highest_down[j] = tree;
                 }
-                let tree = trees[i][len - j - 1];
+                let tree = trees[i * width + len - j - 1];
                 if tree > highest_right {
-                    grid[i][len - j - 1] = 1;
+                    grid[i][len - j - 1] = true;
                     highest_right = tree;
                 }
             }
         }
 
-        grid.into_iter()
-            .map(|v| v.into_iter().sum::<u32>())
-            .sum::<u32>()
-            .into()
+        grid.into_iter().flatten().filter(|&vis| vis).count().into()
     }
 
     fn part_two(&self, input: &str) -> Solution {
-        let trees: Vec<Vec<u8>> = input
-            .lines()
-            .map(|lines| lines.as_bytes().iter().map(|b| b & 0x0f).collect())
-            .collect();
-        let len = trees.len();
+        let trees = input.as_bytes();
+        let len = slice::memchr::memchr(b'\n', trees).unwrap();
+        let width = len + 1;
         let mut high_score = 0;
         for i in 0..len {
             for j in 0..len {
                 let mut score: [u32; 4] = [0; 4];
                 let mut prev = 0;
-                let current_tree = trees[i][j];
+                let current_tree = trees[i * width + j];
                 // sweep up
                 for l in (0..i).rev() {
                     score[0] += 1;
-                    let tree = trees[l][j];
+                    let tree = trees[l * width + j];
                     if tree >= current_tree && tree > prev {
                         break;
                     }
@@ -72,7 +65,7 @@ impl AdventSolver for DayEight {
                 // Sweep down
                 for l in (i + 1)..len {
                     score[1] += 1;
-                    let tree = trees[l][j];
+                    let tree = trees[l * width + j];
                     if tree >= current_tree && tree > prev {
                         break;
                     }
@@ -81,7 +74,7 @@ impl AdventSolver for DayEight {
                 // Sweep left
                 for l in (0..j).rev() {
                     score[2] += 1;
-                    let tree = trees[i][l];
+                    let tree = trees[i * width + l];
                     if tree >= current_tree && tree > prev {
                         break;
                     }
@@ -90,7 +83,7 @@ impl AdventSolver for DayEight {
                 // Sweep right
                 for l in (j + 1)..len {
                     score[3] += 1;
-                    let tree = trees[i][l];
+                    let tree = trees[i * width + l];
                     if tree >= current_tree && tree > prev {
                         break;
                     }
@@ -108,7 +101,7 @@ impl AdventSolver for DayEight {
 }
 
 #[cfg(test)]
-bench! {2022, 8, DayEight, year_2022, Solution::U32(1843), Solution::U32(180_000)}
+bench! {2022, 8, DayEight, year_2022, Solution::Usize(1843), Solution::U32(180_000)}
 
 #[cfg(test)]
 mod tests {
@@ -122,7 +115,7 @@ mod tests {
 ";
     #[test]
     fn part1() {
-        let answer: Solution = 21_u32.into();
+        let answer: Solution = 21_usize.into();
         let day = DayEight {};
         assert_eq!(day.part_one(INPUT), answer);
     }

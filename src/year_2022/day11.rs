2118d94010a2a48ddaab8c357bb8b4678f5f094d
Halvor <halvor.kvamme@gmail.com>
2022-12-11 22:20:47 +0100

    2022 11 - v3 (multithread)

--- src/year_2022/day11.rs
+++ src/year_2022/day11.rs
@@ -1,18 +1,17 @@
+use std::{sync::Arc, thread};
+
 use crate::prelude::*;
 pub struct DayEleven {}
 
-// New = old <op> <u32>
 #[derive(Debug)]
 enum Op {
     Add(u64),
     Mul(u64),
-    // AddOld,
-    MulOld,
+    Square,
 }
 
 #[derive(Debug)]
 struct Monkey {
-    pub items: Vec<u64>,
     pub op: Op,
     pub diviser: u64,
     pub if_true: usize,
@@ -21,26 +20,27 @@ struct Monkey {
 }
 
 #[inline(always)]
-fn collect_monkeys(input: &str) -> Vec<Monkey> {
+fn collect_monkeys(input: &str) -> (Vec<Monkey>, Vec<(u64, usize)>) {
     let mut iter = input.as_bytes().iter();
     let mut monkeys: Vec<Monkey> = Vec::with_capacity(8);
+    let mut items: Vec<(u64, usize)> = Vec::new();
 
     unsafe {
-        for _ in 0..8 {
+        for i in 0..8 {
             // Collect starting values
-            let mut items = Vec::new();
+            // let mut items = Vec::new();
             iter.nth(27).unwrap_unchecked();
-            items.push(
-                ((iter.next().unwrap_unchecked() & 0x0f) * 10
-                    + (iter.next().unwrap_unchecked() & 0x0f)) as u64,
-            );
+            let val = ((iter.next().unwrap_unchecked() & 0x0f) * 10
+                + (iter.next().unwrap_unchecked() & 0x0f)) as u64;
+            items.push((val, i));
+
             loop {
                 let next_byte = iter.next().unwrap_unchecked();
                 if *next_byte == b',' {
                     let val = ((iter.nth(1).unwrap_unchecked() & 0x0f) * 10
                         + (iter.next().unwrap_unchecked() & 0x0f))
                         as u64;
-                    items.push(val);
+                    items.push((val, i));
                 } else {
                     break;
                 }
@@ -52,7 +52,7 @@ fn collect_monkeys(input: &str) -> Vec<Monkey> {
             let op = if *op_val == b'o' {
                 // Skip?
                 iter.nth(2).unwrap_unchecked();
-                Op::MulOld
+                Op::Square
             } else if *op == b'*' {
                 let v_2 = iter.next().unwrap_unchecked();
                 if *v_2 == b'\n' {
@@ -85,9 +85,8 @@ fn collect_monkeys(input: &str) -> Vec<Monkey> {
             let if_false = (iter.nth(31).unwrap_unchecked() & 0x0f) as usize;
 
             iter.next().unwrap_unchecked();
-            iter.next().unwrap_unchecked();
+            iter.next(); //.unwrap_unchecked();
             monkeys.push(Monkey {
-                items,
                 op,
                 diviser,
                 if_true,
@@ -96,31 +95,32 @@ fn collect_monkeys(input: &str) -> Vec<Monkey> {
             });
         }
     }
-    monkeys
+    (monkeys, items)
 }
 impl AdventSolver for DayEleven {
     fn part_one(&self, input: &str) -> Solution {
-        let mut monkeys = collect_monkeys(input);
-
-        let monkey_len = monkeys.len();
+        let (mut monkeys, mut items) = collect_monkeys(input);
         for _ in 0..20 {
-            for i in 0..monkey_len {
-                let monkey = unsafe { &mut *std::ptr::addr_of_mut!(monkeys[i]) };
-                monkey.items.reverse();
-                monkey.inspections += monkey.items.len() as u64;
-                for _ in 0..monkey.items.len() {
-                    let mut level = unsafe { monkey.items.pop().unwrap_unchecked() };
-                    level = match monkey.op {
-                        Op::Add(v) => level + v,
-                        Op::Mul(v) => level * v,
-                        Op::MulOld => level * level,
+            for item in &mut items {
+                loop {
+                    let monkey = unsafe { monkeys.get_unchecked_mut(item.1) };
+                    monkey.inspections += 1;
+                    item.0 = match monkey.op {
+                        Op::Add(v) => item.0 + v,
+                        Op::Mul(v) => item.0 * v,
+                        Op::Square => item.0 * item.0,
                     };
-                    level /= 3;
-                    if level % monkey.diviser == 0 {
-                        monkeys[monkey.if_true].items.push(level);
+                    item.0 /= 3;
+                    let next = if item.0 % monkey.diviser == 0 {
+                        monkey.if_true
                     } else {
-                        monkeys[monkey.if_false].items.push(level);
+                        monkey.if_false
+                    };
+                    if next < item.1 {
+                        item.1 = next;
+                        break;
                     }
+                    item.1 = next;
                 }
             }
         }
@@ -131,31 +131,49 @@ impl AdventSolver for DayEleven {
     }
 
     fn part_two(&self, input: &str) -> Solution {
-        let mut monkeys = collect_monkeys(input);
+        let (monkeys, items) = collect_monkeys(input);
         let modulo: u64 = monkeys.iter().map(|m| m.diviser).product();
-        let monkey_len = monkeys.len();
-        for _ in 0..10000 {
-            for i in 0..monkey_len {
-                let monkey = unsafe { &mut *std::ptr::addr_of_mut!(monkeys[i]) };
-                monkey.items.reverse();
-                monkey.inspections += monkey.items.len() as u64;
-                for _ in 0..monkey.items.len() {
-                    let mut level = unsafe { monkey.items.pop().unwrap_unchecked() };
-                    level = match monkey.op {
-                        Op::Add(v) => level + v,
-                        Op::Mul(v) => level * v,
-                        Op::MulOld => level * level,
-                    };
-                    level %= modulo;
-                    if level % monkey.diviser == 0 {
-                        monkeys[monkey.if_true].items.push(level);
-                    } else {
-                        monkeys[monkey.if_false].items.push(level);
+        let monkeys = Arc::new(monkeys);
+        let handles = items
+            .into_iter()
+            .map(|mut i| {
+                let monkeys = monkeys.clone();
+                thread::spawn(move || -> [u64; 8] {
+                    let mut inspections = [0_u64; 8];
+                    let mut round = 0;
+                    while round < 10_000 {
+                        let monkey = unsafe { monkeys.get_unchecked(i.1) };
+                        *unsafe { inspections.get_unchecked_mut(i.1) } += 1;
+                        i.0 = match monkey.op {
+                            Op::Add(v) => i.0 + v,
+                            Op::Mul(v) => i.0 * v,
+                            Op::Square => i.0 * i.0,
+                        };
+                        i.0 %= modulo;
+                        i.1 = if i.0 % monkey.diviser == 0 {
+                            if i.1 > monkey.if_true {
+                                round += 1;
+                            }
+                            monkey.if_true
+                        } else {
+                            if i.1 > monkey.if_false {
+                                round += 1;
+                            }
+                            monkey.if_false
+                        };
                     }
-                }
+                    inspections
+                })
+            })
+            .collect::<Vec<_>>();
+
+        let mut inspections = [0_u64; 8];
+        for handle in handles {
+            let handle_res = handle.join().unwrap();
+            for i in 0..8 {
+                inspections[i] += handle_res[i];
             }
         }
-        let mut inspections = monkeys.iter().map(|m| m.inspections).collect::<Vec<u64>>();
         inspections.sort_unstable();
         inspections.reverse();
         (inspections[0] * inspections[1]).into()
@@ -165,33 +183,33 @@ impl AdventSolver for DayEleven {
 #[cfg(test)]
 bench! {2022, 11, DayEleven, 119_715_u64, 18_085_004_878_u64}
 
-// #[cfg(test)]
-// test! {DayEleven, 10605_u64, 2_713_310_158_u64, "\
-// Monkey 0:
-//   Starting items: 79, 98
-//   Operation: new = old * 19
-//   Test: divisible by 23
-//     If true: throw to monkey 2
-//     If false: throw to monkey 3
+#[cfg(test)]
+test! {DayEleven, 10605_u64, 2_713_310_158_u64, "\
+Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
 
-// Monkey 1:
-//   Starting items: 54, 65, 75, 74
-//   Operation: new = old + 6
-//   Test: divisible by 19
-//     If true: throw to monkey 2
-//     If false: throw to monkey 0
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
 
-// Monkey 2:
-//   Starting items: 79, 60, 97
-//   Operation: new = old * old
-//   Test: divisible by 13
-//     If true: throw to monkey 1
-//     If false: throw to monkey 3
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
 
-// Monkey 3:
-//   Starting items: 74
-//   Operation: new = old + 3
-//   Test: divisible by 17
-//     If true: throw to monkey 0
-//     If false: throw to monkey 1
-// "}
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1
+"}
